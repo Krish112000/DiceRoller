137f550a3754622b3091e98a0b3ca480303940d8
Krishna <1839574@brunel.ac.uk>
2019-11-08 00:01:53 +0000

    Part 3a included

---
+++ app/src/main/java/com/example/diceroller1/Add_DICEBREAKER.java
@@ -0,0 +1,14 @@
+package com.example.diceroller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_DICEBREAKER extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__dicebreaker);
+    }
+}
--- app/src/main/java/com/example/diceroller1/MainActivity.java
+++ app/src/main/java/com/example/diceroller1/MainActivity.java
@@ -15,11 +15,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int s1= 0;
+    int s1= 0; //defined here so that it does not return to 0 everytime the button is clicked.
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +62,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void on_button_click(View view){
-        EditText input = (EditText)findViewById(R.id.EnterNumber);
-        int ui= Integer.parseInt(input.getText().toString());
+    public void on_button_click(View view) {
+        //Allows to the user to input a value
+        EditText input = (EditText) findViewById(R.id.EnterNumber);
+        int ui = Integer.parseInt(input.getText().toString());
 
-        TextView tv= this.findViewById(R.id.numberTextView);
+        if ((ui >6)||(ui<1)){
+            throw new IllegalArgumentException("Input number betwwen 1-6");
+        }
+
+        TextView tv = this.findViewById(R.id.numberTextView);
+
+        //Generates random number
         Random r = new Random();
-        int number = r.nextInt(6)+1;
-        tv.setText (Integer.toString(number));
-        TextView s= this.findViewById(R.id.Score);
+        int number = r.nextInt(6) + 1;
+
+        tv.setText(Integer.toString(number));
+        TextView s = this.findViewById(R.id.Score);
         s.setText(Integer.toString(s1));
-       // s1++;
+        // s1++;
 
         if (ui == number) {
             s1++;
@@ -78,8 +88,60 @@ public class MainActivity extends AppCompatActivity {
 
         }
         else {
-           // Allow the user to continue rolling the dice
+            // Allow the user to continue rolling the dice
+        }
+    }
+
+    public void onClick (View v){
+        TextView tv = this.findViewById(R.id.numberTextView);
+
+        //Generates random number
+        Random r2 = new Random();
+        int number = r2.nextInt(6) + 1;
+
+        if (number == 1) {
+
+            tv.setText("If you could go anywhere in the world, where would you go?");
+
+        }
+        else if (number == 2) {
+
+            tv.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+        }
+        else if (number == 3){
+            tv.setText("If you could eat only one food for the rest of your life, what would that be?");
+
+        }
+        else if (number == 4){
+            tv.setText("If you won a million dollars, what is the first thing you would buy?");
+
+        }
+        else if (number == 5){
+            tv.setText("If you could spend the day with one fictional character, who would it be?");
+        }
+        else if (number ==6){
+            tv.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
         }
 
+
+
     }
+         /*List<String> Q = new ArrayList<>();
+        Q.add ("If you could go anywhere in the world, where would you go?");
+        Q.add ("If you were stranded on a desert island, what three things would you want to take with you?");
+        Q.add ("If you could eat only one food for the rest of your life, what would that be?");
+        Q.add ("If you won a million dollars, what is the first thing you would buy?");
+        Q.add ("If you could spend the day with one fictional character, who would it be?");
+        Q.add ("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+
+        Random r2 = new Random();
+        int num = r2.nextInt(6)+1;
+
+    }
+    public void roll_the_Dice(){
+        TextView tv= this.findViewById(R.id.numberTextView);
+        Random r = new Random();
+        int number = r.nextInt(6)+1;
+        tv.setText (Integer.toString(number));
+    }*/
 }
